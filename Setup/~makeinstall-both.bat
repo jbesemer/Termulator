77b8d9c4ff730e72c14c3b12dabf19f3d6188f16
JB <james.besemer@gmail.com>
2017-05-11 15:46:16 -0700

    makeinstall both and new pw for new cert

---
+++ Setup/~makeinstall-both.bat
@@ -0,0 +1,8 @@
+set INNO=C:\Program Files (x86)\Inno Setup 5\Compil32.exe
+set MAKE_DEBUG=MakeInstall Debug.iss
+set MAKE_RELEASE=MakeInstall Release.iss
+rem http://www.jrsoftware.org/ishelp/index.php?topic=compilercmdline
+
+"%INNO%" /cc "%MAKE_DEBUG%"
+"%INNO%" /cc "%MAKE_RELEASE%"
+pause
