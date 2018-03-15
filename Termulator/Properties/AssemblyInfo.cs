8617eb8e78c6f3b30e4e4dfab907d7f6164ce027
jbesemer <james.besemer@gmail.com>
2018-03-15 00:14:42 -0700

    2018 copyright

--- Termulator/Properties/AssemblyInfo.cs
+++ Termulator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Coherent" )]
 [assembly: AssemblyProduct( "Termulator" )]
-[assembly: AssemblyCopyright( "Copyright © Coherent 2012-2017" )]
+[assembly: AssemblyCopyright( "Copyright © Coherent 2012-2018" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
