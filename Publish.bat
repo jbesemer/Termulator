7fce6ce8388c4caac368068374ee43f460a18b63
Besemer, Jim <Jim.Besemer@coherent.com>
2018-07-19 16:26:39 -0700

    fix pub.bat

--- Publish.bat
+++ Publish.bat
@@ -1,10 +1,10 @@
 set DEST=C:\Program Files (x86)\Coherent\Termulator\
-set BIN=Terminal\bin\Debug
+set BIN=Termulator\bin\Debug
 set PLAYERBIN=C:\Program Files (x86)\SoundPlayer\
 set SOUNDS=C:\Windows\Media\Sounds\SmallSounds\
 
-copy "%BIN%\Terminal.exe" "%DEST%" || goto :error
-copy "%BIN%\Terminal.exe.config" "%DEST%" || goto :error
+copy "%BIN%\Termulator.exe" "%DEST%" || goto :error
+copy "%BIN%\Termulator.exe.config" "%DEST%" || goto :error
 copy "%BIN%\Termulator.pdf" "%DEST%" || goto :error
 
 "%PLAYERBIN%Player.exe" "%SOUNDS%DRIP.WAV"
