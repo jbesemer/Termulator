eaf17d7cc59cf5cacc96662dd76a08c2210f59f7
Besemer, Jim <Jim.Besemer@coherent.com>
2020-01-08 12:36:48 -0800

    improved Setup, Publish

--- Setup/Publish.bat
+++ Setup/Publish.bat
@@ -1,11 +1,18 @@
-set DEST=C:\Program Files (x86)\Coherent\Termulator\
+set DEST=C:\Program Files (x86)\Coherent\Termulator
 set BIN=Termulator\bin\Debug
+set DOC=DOC
 set PLAYERBIN=C:\Program Files (x86)\SoundPlayer\
 set SOUNDS=C:\Windows\Media\Sounds\SmallSounds\
 
+cd ..
+
 copy "%BIN%\Termulator.exe" "%DEST%" || goto :error
 copy "%BIN%\Termulator.exe.config" "%DEST%" || goto :error
-copy "%BIN%\Termulator.pdf" "%DEST%" || goto :error
+copy "%BIN%\SharedLibrary.dll" "%DEST%" || goto :error
+copy "%BIN%\Library.dll" "%DEST%" || goto :error
+copy "%BIN%\Newtonsoft.Json.dll" "%DEST%" || goto :error
+copy "%BIN%\Newtonsoft.Json.xml" "%DEST%" || goto :error
+copy "%DOC%\Termulator.pdf" "%DEST%" || goto :error
 
 "%PLAYERBIN%Player.exe" "%SOUNDS%DRIP.WAV"
 
