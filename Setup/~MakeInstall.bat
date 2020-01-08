599dab890c00dab5f6d1cc8b30a66a6357ee68d9
Besemer, Jim <Jim.Besemer@coherent.com>
2020-01-08 12:41:23 -0800

    new publish

--- Setup/~MakeInstall.bat
+++ Setup/~MakeInstall.bat
@@ -1,13 +1,13 @@
 set PLAYERBIN=C:\Program Files (x86)\SoundPlayer\
 set SOUNDS=C:\Windows\Media\Sounds\SmallSounds\
 set DEST=C:\Program Files (x86)\Coherent\Termulator\
-set SRC=Termulator\bin\Debug
+set BIN=Termulator\bin\Debug
 set DOC=Doc
 
-xcopy /y %SRC%\Termulator.exe "%DEST%" 				|| goto :error
-xcopy /y %SRC%\Library.dll "%DEST%" 						|| goto :error
-xcopy /y %SRC%\Termulator.exe.config "%DEST%" 	|| goto :error
-xcopy /y %DOC%\Termulator.pdf "%DEST%" 				|| goto :error
+xcopy /y %BIN%\Termulator.exe "%DEST%" 		|| goto :error
+xcopy /y %BIN%\Library.dll "%DEST%" 			|| goto :error
+xcopy /y %BIN%\Termulator.exe.config "%DEST%" 	|| goto :error
+xcopy /y %DOC%\Termulator.pdf "%DEST%" 		|| goto :error
 
 "%PLAYERBIN%Player.exe" "%SOUNDS%DRIP.WAV"
 
